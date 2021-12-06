7fe30982a2ccc6cc0b7c694cd3cfe0be0b2016a1
Monalmo <cristian.hadad.g@gmail.com>
2021-12-06 00:23:25 -0300

    base

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.main
+	Nuxt
+</template>
+
+<style lang="sass">
+@import '~/estilos/colores'
+@import '~/estilos/paleta'
+
+.main
+	min-height: 100vh
+	// overflow-y: auto
+	position: relative
+	// border: 3px solid green
+	display: flex
+	flex-flow: column nowrap
+	.header
+		flex: auto 0 0
+	.cuerpo
+		flex: auto 1 0
+		overflow-x: hidden
+	.footer
+		flex: auto 0 0
+</style>
