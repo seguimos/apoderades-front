9acf96843b03adeb9bbc86bfb991242682c9922b
Camilo Núñez <camonunez@gmail.com>
2021-12-07 19:49:35 -0300

    Descripción del consolo

--- plugins/consolo.js
+++ plugins/consolo.js
@@ -1,5 +1,7 @@
 import consolo from '@mainlib/consolo'
 
+// El consolo es como la consola, pero solo funciona (log, info, error, etc) en desarrollo
+
 export default ({ app }, inject) => {
 	inject('consolo', consolo)
 }
