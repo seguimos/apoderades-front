6d110b2f173bcb2fa2d315440126c6e00ececb82
Monalmo <cristian.hadad.g@gmail.com>
2021-12-10 03:55:23 -0300

    avances formularios

--- pages/app/apoderades.vue
+++ pages/app/apoderades.vue
@@ -1,10 +1,18 @@
 <template lang="pug">
 .root
-	.formularioValidacion
-		h1 Falta validar
-		validar
+	apoderades-validar(v-if="!$back.apoderade.fechaValidacionDatos")
+	.buscarApoderade(v-else)
+		apoderades-buscar
 </template>
 <script>
-export default {}
+export default {
+	mounted () {
+		if (!this.$back.apoderade.fechaValidacionDatos) return
+		const ap = this.$back.apoderade.rol
+		if (ap === 'AM') {
+			this.$router.replace('/app/locales')
+		} else return true
+	}
+}
 </script>
 <style lang="sass" scoped></style>
