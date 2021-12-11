de4fbe0ebd4781d9790e2bc7f3e486b2a6aee804
Eduardo <e.laboral@xor.cl>
2021-12-10 21:54:15 -0300

    arreglar navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,14 +11,12 @@ a-layout.root
 				.logo-after
 					.iconoAprueboDignidad
 		a-menu(mode="horizontal")
-			a-menu-item(key="login", v-if="!$usuario").header-menu
-				n-link.link(to="/app/login") Iniciar Sesión
 			a-sub-menu(v-if="$usuario")
 				span.submenu-title-wrapper(slot="title")
 					a-icon(type="user")
 					| Cuentas
 				a-menu-item(v-if="$usuario", key="mi-cuenta")
-					n-link.link(to="/cuenta") Mi cuenta
+					n-link.link(to="/app") Mi cuenta
 				a-menu-item(v-if="!$usuario", key="mi-cuenta")
 					n-link.link(to="/app") Iniciar Sesion
 				a-menu-item(key="mis-datos")
@@ -49,7 +47,7 @@ a-layout.root
 					a-menu-item(key="datos-votacion")
 						a(href="https://consulta.servel.cl") Datos Votación
 					a-menu-item(key="resultados")
-						| Resultados
+						a(href="https://resultados.servel.cl") Datos Votación
 					a-menu-item(key="reglamento")
 						| Reglamento
 				a-menu-item-group(title="Comando")
